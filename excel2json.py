a954b1ea2be47367047387efb7b1ee41877a0818
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2017-04-23 23:09:12 -0700

    add index to json, add comments

--- excel2json.py
+++ excel2json.py
@@ -8,33 +8,33 @@ def stateAbbDict():
     with open('statesDict.txt') as f:
         for line in f.read().split('\n'):
             stateAbbr = line.strip().split(":")
-            # print(stateAbbr)
             stateDict[stateAbbr[0].upper()] = stateAbbr[1].upper().strip()
     return stateDict
 
 stateDict = stateAbbDict()
-# print(stateDict)
-df = pandas.read_excel(io='./Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls')
+
+# Cleaning
+print("Generating Json file from xls file")
+filename = './Table_8_Offenses_Known_to_Law_Enforcement_by_State_by_City_2015.xls'
+df = pandas.read_excel(io = filename)
 newHeader = df.iloc[2]
-df = df[3:]
-
+df = df[3:] # delete xls header
 df.columns = newHeader
-
 df = df.drop(df.index[range(-10,0)]) # deleting comments in EXEL
-
 numOfCities = df.shape[0]
 
 for i in range(1,df.shape[0]):
     if type(df.iloc[i]["State"]) != str:
-        df.iloc[i]["State"] = df.iloc[i-1]["State"]
-        #df.iloc[i]["City"] = re.findall(r"([a-zA-Z]*[\_?])*", df.iloc[i]["City"])[0]
+        df.iloc[i]["State"] = df.iloc[i-1]["State"] # fill state column for all cities
         if re.search(r"[0-9]", df.iloc[i]["City"]) != None:
             df.iloc[i]["City"] = df.iloc[i]["City"][:-1]
+            # solve issue "Santa Barbara5"
     else:
         pass
     # dumb way?
 df["State"] = df["State"].apply(lambda state: stateDict[state])
 
+# Calculate violent crime rate for each city in df
 def getCriRate(row):
     try:
         return float(row['Violent\ncrime']) / row['Population'] * 100 #df.ix[1:10,'Population']
@@ -47,8 +47,6 @@ avgCriRate = df.ix[:,'Crime Rate'].mean()
 USPOP = df.ix[:,"Population"].sum()
 
 df = df.sort_values(by=['Crime Rate','Population'], ascending = [True,False])
-# df['Weighted Crime Rate'] = df['Crime Rate']
-# df['Crime Index'] = df['Weighted Crime Rate']
 df['Crime Ranking'] = np.nan
 df['Crime Index'] = np.nan
 popInSaferCities = 0.0
@@ -57,6 +55,8 @@ currentCriRate = 0.0
 # calculate crime ranking & index by pop
 for i in range(0,numOfCities): 
     # -1: Crime Index -2: Crime Ranking -3: Crime Rate
+    # Crime Index: Crime Rate / mean Crime Rate
+    # Crime Ranking: safter than % of people in U.S.
     df.iloc[i,-2] = "{0:.2f}".format((USPOP - popInSaferCities) / USPOP * 100)
     df.iloc[i,-3] = "{0:.2f}".format(df.iloc[i,-3])
     df.iloc[i,-1] = "{0:.2f}".format(float(df.iloc[i,-3]) / avgCriRate * 100)
@@ -66,10 +66,14 @@ for i in range(0,numOfCities):
 
 df = df.drop(df.columns[range(4,14)], 1)
 df = df.drop(["Population","Violent\ncrime"], 1)
-print(df[9000:9005])
-
+df['Index'] = range(0,numOfCities) # add index for city(json file need index)
+# to json
 outDict = df.set_index('City').T.to_dict()
-# print(outDict.keys())
 out = json.dumps(outDict, sort_keys=True,indent=4, separators=(',', ': '))
 with open('crimeByCity.json','w') as f:
     f.write(out)
+
+
+# df['Weighted Crime Rate'] = df['Crime Rate']
+# df['Crime Index'] = df['Weighted Crime Rate']
+print("Json file created: crimeByCity.json")
